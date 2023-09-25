32757eb0f59c77e18a0e0233b5d44f909f590855
Bartosz Szewczyk <bszewczyk@intype.pl>
2023-09-25 12:42:19 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Flappy Pepe',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
