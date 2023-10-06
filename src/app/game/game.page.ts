1143658c8d5984208d778cca5dc050bf73c2efde
Bartosz Szewczyk <bszewczyk@intype.pl>
2023-10-06 21:42:25 +0200

    Refactoring and animation

--- src/app/game/game.page.ts
+++ src/app/game/game.page.ts
@@ -3,17 +3,20 @@ import 'phaser';
 
 let game:any;
 const deviceParameters = {
-  width: 800,
+  width: 1200,
   height: 600
 }
-const VELOCITY:number = 200;
-const FLAP_VELOCITY:number = 150;
+const VELOCITY:number = 300;
+
+const FLAP_VELOCITY:number = 199;
 const PIPES_TO_RENDER = 128;
 const _initialPepePosition = {
   x: deviceParameters.width * 0.1,
   y: deviceParameters.height / 2,
 }
-const pipeVerticalDistanceRange = [280, 450];
+const pipeVerticalDistanceRange = [100, 250];
+let pipeHorizontalDistance: number = 0;
+
 
 @Component({
   selector: 'app-game',
@@ -29,7 +32,7 @@ export class GamePage implements OnInit {
     physics: {
       default: 'arcade',
       arcade: {
-        debug: false,
+        debug: true,
       }
     },
     scene: {
@@ -50,23 +53,22 @@ export class GamePage implements OnInit {
   pipeVerticalPosition:number | undefined;
   upperPipe: any;
   lowerPipe:any;
-  
+
   constructor() {
     game = new Phaser.Game(this.config);
   }
 
   preload () {
     this.load.image('sky', './assets/sky.png');
-
     this.load.image('pepeup', './assets/pepeup.png');
+    this.load.image('pepeup_fast', './assets/pepeup_fast.png');
     this.load.image('pepedown', './assets/pepedown.png');
-
+    this.load.image('pepedown_slow', './assets/pepedown_slow.png');
     this.load.image('pipe', './assets/pipe.png');
   }
 
   create () {
     this.add.image(0, 0, 'sky').setOrigin(0);
-
     this.pepe = this.physics.add.sprite(_initialPepePosition.x, _initialPepePosition.y, 'pepedown').setOrigin(0);
     this.pepe.body.gravity.y = 400;
     this.pepe.displayWidth = 42;
@@ -75,19 +77,15 @@ export class GamePage implements OnInit {
     this.input.on('pointerdownoutside', () => {
       flap(this.pepe.body.velocity);
     });
-    let pipeHorizontalDistance:number = 0;
+
     for (let i = 0; i < PIPES_TO_RENDER; i++) {
-      this.pipeVerticalDistance = Phaser.Math.Between(pipeVerticalDistanceRange[0], pipeVerticalDistanceRange[1]);
-      this.pipeVerticalPosition = Phaser.Math.Between(0 + 20, game.config.height - 20 - this.pipeVerticalDistance);
-      this.upperPipe = this.physics.add.sprite(pipeHorizontalDistance, this.pipeVerticalPosition, 'pipe').setOrigin(0, 1);
-      this.lowerPipe = this.physics.add.sprite(this.upperPipe.body.x+30, this.upperPipe.body.y + this.pipeVerticalDistance,  'pipe').setOrigin(0, 0);
-      this.upperPipe.body.velocity.x = -200;
-      this.lowerPipe.body.velocity.x = -200;
-      pipeHorizontalDistance = pipeHorizontalDistance + 400;
+      const upperPipe = this.physics.add.sprite(0, 0, 'pipe').setOrigin(0, 1);
+      const lowerPipe = this.physics.add.sprite(0, 0,  'pipe').setOrigin(0, 0);
+      placePipe(upperPipe, lowerPipe);
     }
   }
 
-  update(){
+  update(timer){
     if(this.pepe.body.position.y >= game.config.height-this.pepe.displayHeight){
       this.pepe.body.velocity.y = -VELOCITY;
       gameOver(this.pepe);
@@ -96,11 +94,18 @@ export class GamePage implements OnInit {
       gameOver(this.pepe);
     }
 
-    if(this.pepe.body.velocity.y >= 0){
-      this.pepe.setTexture('pepedown');
+    if(this.pepe.body.velocity.y >= 0 || this.pepe.body.velocity.y > -25){
+      this.pepe.setTexture('pepedown_slow');
+      if(this.pepe.body.velocity.y > 180){
+        this.pepe.setTexture('pepedown');
+      }
     }else{
       this.pepe.setTexture('pepeup');
+      if(this.pepe.body.velocity.y < -60){
+        this.pepe.setTexture('pepeup_fast');
+      }
     }
+
   }
   ngOnInit() {}
 }
@@ -113,4 +118,24 @@ function gameOver(player:any):void {
 
 function flap(playerVelocity:any):void{
   playerVelocity.y =- FLAP_VELOCITY;
+}
+
+function placePipe(uPipe:any, lPipe:any){
+  pipeHorizontalDistance += 400;
+
+  let pipeVerticalDistance = Phaser.Math.Between(pipeVerticalDistanceRange[0], pipeVerticalDistanceRange[1]);
+  let pipeVerticalPosition = Phaser.Math.Between(0 + 20, game.config.height - 20 - pipeVerticalDistance);
+
+  uPipe.x = pipeHorizontalDistance;
+  uPipe.y = pipeVerticalPosition;
+
+  lPipe.x = uPipe.x;
+  lPipe.y = uPipe.y + pipeVerticalDistance;
+
+  uPipe.body.velocity.x = -300;
+  lPipe.body.velocity.x = -300;
+
+  console.log(pipeHorizontalDistance);
+
+
 }
